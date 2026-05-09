f488b1ea1d72d6414b96f27d6383b9507a28e935
gehadepada <gehadepada@gmail.com>
2026-05-09 19:57:12 +0300

    put Find Pivot Index solution

---
+++ Topic 4:Prefix Sum/Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int rightsum=accumulate(nums.begin(),nums.end(),0);
+        int leftsum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            rightsum-=nums[i];
+            if(rightsum==leftsum)
+            return i;
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
