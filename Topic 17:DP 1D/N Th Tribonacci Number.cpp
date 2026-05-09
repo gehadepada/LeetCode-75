1f3737ccb95922d27bfa533f1223878b0859bf79
gehadepada <gehadepada@gmail.com>
2026-05-09 19:59:21 +0300

    put N Th Tribonacci Number solution

---
+++ Topic 17:DP 1D/N Th Tribonacci Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+    
+       long long arr[38];
+       
+            arr[0]=0;
+            arr[1]=1;
+            arr[2]=1;
+        for(int i=3;i<=37;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+
+
+
+        }
+        return arr[n];
+        
+
+        
+    }
+};
